3037e892406d19d2cd463e4a749838a22d1c50d8
wawalid <walidtoubi89@gmail.com>
2025-07-10 17:29:26 +0200

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,31 +5,23 @@ function Dashboard() {
 
 
 
-  return (
-    <nav className="bg-zinc-800 my-3 flex justify-between py-5 px-10 rounded-lg">
-      <Link to={"/"}>
-        <h1 className="text-2xl font-bold">Nutridash</h1>
-      </Link>
-      </nav>
-      <div>
-      <ul className="flex gap-x-5 text-white">
-      <div className="container-columnas">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <p>columna donde ira los macros</p>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <p>columna donde ira las calorias</p>
-          </div>
+ return (
+  <>
+    <div className="container-columnas grid grid-cols-1 md:grid-cols-2 gap-8 mb-4">
+      <div className="bg-gray-700 p-4 rounded-xl shadow">
+        <p>Columna donde irán los macros</p>
       </div>
-      <div className="container-historial bg-white shadow p-4 rounded-xl">
+      <div className="bg-gray-700 p-4 rounded-xl shadow">
+        <p>Columna donde irán las calorías</p>
+      </div>
+    </div>
 
-      </div>
-      
-    </ul>
-      </div>
-      
-    
-  );
+    <div className="container-historial bg-gray-700 shadow p-4 rounded-xl">
+      <p>Historial de consumo / actividad</p>
+    </div>
+  </>
+);
+
 }
 
 export default Dashboard;
