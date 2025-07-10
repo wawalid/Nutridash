a822cf06ffec606dd84d2c869dfdbe621c5a209c
wawalid <walidtoubi89@gmail.com>
2025-07-10 18:19:24 +0200

    un par de cambios

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/merndb";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/nutridash";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "fallback-secret-key";
 export const PORT = process.env.PORT || 4000;
 
